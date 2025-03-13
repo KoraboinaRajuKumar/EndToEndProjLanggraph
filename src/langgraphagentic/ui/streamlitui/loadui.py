4692b41cc5b5c2164d249ebfc78f8867ff7bac68
koraboinaraju <rajujava0355@gmail.com>
2025-03-13 21:42:10 +0530

    loadui added

---
+++ src/langgraphagentic/ui/streamlitui/loadui.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import os
+from datetime import datetime
+from langchain_core.messages import HumanMessage,AIMessage
+
+from src.langgraphagentic.ui.uiconfig import Config
+
+class LoadStreamlitUi():
+    def __init__(self):
+        self.config=Config()
+
+
+
+
+
